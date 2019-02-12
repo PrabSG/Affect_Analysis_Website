4e11e49b53f12a4573fc076449b2907953e0712c
Calin Biberea <calinbiberea@gmail.com>
2019-02-12 11:05:02 +0000

    Added a gallery, creating work oriented website

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+var images = document.querySelectorAll("#slideshow figure");
+for(i=0; i < images.length; i++) {
+  images[i].addEventListener('click',
+  function(){
+    this.classList.toggle("expanded");
+    quad.classList.toggle("full")
+  });
+}
